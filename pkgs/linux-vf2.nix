e802304b75bbd36f2fefced6dcd75d76176838e2
Robin Voetter <robin@voetter.nl>
2023-05-27 22:51:59 +0200

    linux: update to starfive 6.4

--- pkgs/linux-vf2.nix
+++ pkgs/linux-vf2.nix
@@ -1,34 +1,47 @@
 { lib , fetchFromGitHub , buildLinux, fetchpatch, ... } @ args:
 let
-  modDirVersion = "6.2.0";
+  modDirVersion = "6.4.0-rc3";
 in
 buildLinux (args // {
   inherit modDirVersion;
   version = "${modDirVersion}-vf2";
 
   src = fetchFromGitHub {
-    owner = "esmil";
+    owner = "starfive-tech";
     repo = "linux";
-    rev = "f3df0dfa0962172885dda035fbc579c418b78519";
-    sha256 = "sha256-mfI8RB/T0r/PkEH1eJTZ4ta0GRu7vbU6sd0DqhVJtfQ=";
+    rev = "d2637628afd75af2064e74fd1b81fb4a97a76a92";
+    hash = "sha256-39yhOsHP3RP75Mge3Zhktj6+oIwTHQZlOb82jjM2Hbg=";
   };
 
   structuredExtraConfig = with lib.kernel; {
+    ARCH_STARFIVE = yes;
     SOC_STARFIVE = yes;
-    SOC_STARFIVE_JH7110 = yes;
-    CLK_STARFIVE_JH7110_SYS = yes;
-    RESET_STARFIVE_JH7110 = yes;
-    PINCTRL_STARFIVE_JH7110 = yes;
-    SERIAL_8250_DW = yes;
 
-    # (apparently) doesn't work as module
-    SPI_PL022 = yes;
-    SPI_PL022_STARFIVE = yes;
-    RTC_DRV_STARFIVE = yes;
+    SERIAL_8250 = yes;
+
+    NO_HZ_IDLE = yes;
+    CPU_FREQ = yes;
+    CPUFREQ_DT = yes;
+    CPUFREQ_DT_PLATDEV = yes;
+    HIBERNATION = yes;
+
+    GPIO_SYSFS = yes;
+    POWER_RESET_GPIO_RESET = yes;
+
+    PROC_KCORE = yes;
+
+    PWM = yes;
+    PWM_STARFIVE_PTC = yes;
+
+    SIFIVE_CCACHE = yes;
+
+    V4L_PLATFORM_DRIVERS = yes; # TODO: Make module
+
+    PL330_DMA = no;
   };
 
   extraMeta = {
-    branch = "jh7110";
+    branch = "JH7110_VisionFive2_upstream";
     description = "Linux kernel for StarFive's VisionFive2";
     platforms = [ "riscv64-linux" ];
   };
