cc712bfc384e548db965f6807d90988d678c87bc
Robin Voetter <robin@voetter.nl>
2024-02-11 00:39:17 +0100

    new version

--- flake.nix
+++ flake.nix
@@ -2,24 +2,83 @@
   description = "VisionFive 2 test flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs";
 
-    ubootSrc = {
+    uboot-src = {
       flake = false;
       url = "github:u-boot/u-boot";
     };
+
+    starfive-tools-src = {
+      flake = false;
+      url = "github:starfive-tech/Tools";
+    };
   };
 
-  outputs = { self, nixpkgs, ubootSrc }: rec {
+  outputs = { self, nixpkgs, uboot-src, starfive-tools-src }: rec {
     overlays.default = self: super: {
-      # glib is broken
-      util-linux = super.util-linux.override { translateManpages = false; };
-      # Dont need this
-      neofetch = super.neofetch.override { x11Support = false; };
       # Flaky tests
-      openssh = super.openssh.overrideAttrs (old: {
-        doCheck = false;
+      coreutils = super.coreutils.overrideAttrs (old: { doCheck = false; });
+      coreutils-full = super.coreutils-full.overrideAttrs (old: { doCheck = false; });
+      diffutils = super.diffutils.overrideAttrs (old: { doCheck = false; });
+      gnugrep = super.gnugrep.overrideAttrs (old: { doCheck = false; });
+      libuv = super.libuv.overrideAttrs (old: { doCheck = false; });
+      libseccomp = super.libseccomp.overrideAttrs (old: { doCheck = false; });
+      bind = super.bind.overrideAttrs (old: { doCheck = false; });
+      elfutils = super.elfutils.overrideAttrs (old: { doCheck = false; doInstallCheck = false; });
+
+      nixVersions = super.nixVersions // {
+        nix_2_18 = super.nixVersions.nix_2_18.overrideAttrs (old: { doCheck = false; doInstallCheck = false; });
+        nix_2_15 = super.nixVersions.nix_2_15.overrideAttrs (old: { doCheck = false; });
+      };
+      nix = self.nixVersions.nix_2_18;
+
+      python3 = super.python3.override {
+        packageOverrides = pyself: pysuper: {
+          psutil = pysuper.psutil.overrideAttrs (old: { dontUsePytestCheck = true; });
+          sphinx = pysuper.sphinx.overrideAttrs (old: {
+            disabledTests = old.disabledTests ++ [
+              "test_connect_to_selfsigned_with_tls_cacerts"
+              "test_connect_to_selfsigned_fails"
+            ];
+          });
+        };
+      };
+      pythonPackages = self.python3.pkgs;
+
+      # https://github.com/systemd/systemd/issues/30448
+      systemd = super.systemd.overrideAttrs (old: {
+        env.NIX_CFLAGS_COMPILE = toString ([old.env.NIX_CFLAGS_COMPILE "-Wno-error=format-overflow" ]);
       });
+      systemdMinimal = super.systemdMinimal.overrideAttrs (old: {
+        env.NIX_CFLAGS_COMPILE = toString ([old.env.NIX_CFLAGS_COMPILE "-Wno-error=format-overflow" ]);
+      });
+
+      # Im not sure why this fails. The CMake script seems correct... further investigation is needed.
+      zeromq = super.zeromq.overrideAttrs (old: {
+        postPatch = old.postPatch + ''
+          substituteInPlace CMakeLists.txt \
+                --replace 'set(ZMQ_CACHELINE_SIZE ''${CACHELINE_SIZE})' 'set(ZMQ_CACHELINE_SIZE 64)'
+        '';
+      });
+
+      # llvm 16 fails to build. Also, save some work by not compiling llvm 16 at all.
+      # This replaces the default llvm toolchain with llvm 17
+      inherit (self.llvmPackages_17) libclang clang clang-manpages lld lldb llvm libllvm llvm-manpages libcxx libcxxabiI;
+      clang-tools = self.clang-tools_17;
+
+      buildPackages = super.buildPackages // {
+        llvmPackages = self.llvmPackages_17;
+        inherit (self.llvmPackages_17) libclang clang clang-manpages lld lldb llvm libllvm llvm-manpages libcxx libcxxabiI;
+        clang-tools = self.clang-tools_17;
+      };
+
+      # We don't want X.
+      graphviz = super.graphviz.override { withXorg = false; };
+      gd = super.gd.override { withXorg = false; };
+      pango = super.pango.override { x11Support = false; withIntrospection = false; };
+      gdk-pixbuf = super.gdk-pixbuf.override { withIntrospection = false; };
+      cairo = super.cairo.override { x11Support = false; };
     };
 
     nixosModules = {
@@ -37,9 +96,7 @@
 
           nixpkgs = {
             overlays = [ self.overlays.default ];
-
-            localSystem.config = "x86_64-linux";
-            crossSystem.config = "riscv64-linux";
+            hostPlatform = "riscv64-linux";
           };
 
           hardware.deviceTree.name = "starfive/jh7110-starfive-visionfive-2-v1.2a.dtb";
@@ -60,19 +117,27 @@
               "console=ttyS0,115200"
               "earlycon=sbi"
               "boot.shell_on_fail"
+              "clk_ignore_unused" # Causes a hang on some kernels
             ];
-
-            initrd.includeDefaultModules = false;
+            consoleLogLevel = 7;
             initrd.availableKernelModules = [
-              "dw_mmc-pltfm"
               "dw_mmc-starfive"
+              "motorcomm"
               "dwmac-starfive"
-              "spi-dw-mmio"
-              "mmc_block"
+              "cdns3-starfive"
+              "jh7110-trng"
+              "phy-jh7110-usb"
+              "clk-starfive-jh7110-aon"
+              "clk-starfive-jh7110-stg"
+              "clk-starfive-jh7110-vout"
+              "clk-starfive-jh7110-isp"
+              "clk-starfive-jh7100-audio"
+              "phy-jh7110-pcie"
+              "pcie-starfive"
               "nvme"
-              "sdhci"
-              "sdhci-pci"
-              "sdhci-of-dwcmshc"
+            ];
+            blacklistedKernelModules = [
+              "jh7110-crypto" # Crashes
             ];
 
             loader = {
@@ -103,7 +168,7 @@
             users.root.password = "secret";
           };
 
-          system.stateVersion = "23.05";
+          system.stateVersion = "23.11";
 
           environment.systemPackages = with pkgs; [
             neofetch
@@ -111,63 +176,74 @@
             pciutils
             parted
             git
+            nixos-install
+            lm_sensors
           ];
         })
       ];
     };
 
     packages.x86_64-linux = let
-      pkgs-cross = import nixpkgs {
-        localSystem.config = "x86_64-linux";
-        crossSystem.config = "riscv64-linux";
-      };
-      pkgs = import nixpkgs {
-        system = "x86_64-linux";
-      };
+      pkgs = nixpkgs.legacyPackages.riscv64-linux.extend self.overlays.default;
     in rec {
-      sd = nixosConfigurations.sd.config.system.build.sdImage;
-      kernel = pkgs-cross.callPackage ./pkgs/linux-vf2.nix { kernelPatches = [ ]; };
-      splTool = pkgs.callPackage ./pkgs/spl_tool.nix { };
-
-      uboot = pkgs-cross.buildUBoot {
-        # version = "2023.8";
-        # src = ubootSrc;
+      uboot = (pkgs.buildUBoot {
+        version = uboot-src.shortRev;
+        src = uboot-src;
         defconfig = "starfive_visionfive2_defconfig";
         filesToInstall = [
-          "u-boot.bin"
+          "u-boot.itb"
           "spl/u-boot-spl.bin"
-          "arch/riscv/dts/starfive_visionfive2.dtb"
         ];
-      };
-
-      opensbi = (pkgs-cross.opensbi.override {
-        withPayload = "${uboot}/u-boot.bin";
-        withFDT = "${uboot}/starfive_visionfive2.dtb";
+        makeFlags = [
+          "OPENSBI=${pkgs.opensbi}/share/opensbi/lp64/generic/firmware/fw_dynamic.bin"
+        ];
       }).overrideAttrs (old: {
+        # some RPI patch doesn't work
+        patches = [];
+      });
+
+      opensbi = pkgs.opensbi.overrideAttrs (old: {
         makeFlags = old.makeFlags ++ [ "FW_TEXT_START=0x40000000" ];
       });
 
-      firmware = pkgs-cross.stdenvNoCC.mkDerivation {
+      firmware = pkgs.stdenvNoCC.mkDerivation {
         name = "firmware-vf2";
         dontUnpack = true;
-        nativeBuildInputs = [ splTool pkgs.dtc pkgs.ubootTools ];
+        nativeBuildInputs = [ spl-tool pkgs.dtc pkgs.ubootTools ];
         installPhase = ''
           runHook preInstall
 
           mkdir -p "$out/"
-          cp ${uboot}/u-boot-spl.bin u-boot-spl.bin
-          spl_tool -c -f ./u-boot-spl.bin -v 0x01010101
-          mv ./u-boot-spl.bin.normal.out "$out/"
-          rm ./u-boot-spl.bin
 
-          # TODO: Maybe we can fetch this image directly from github too.
-          substitute ${./visionfive2-uboot-fit-image.its} visionfive2-uboot-fit-image.its \
-            --replace fw_payload.bin ${opensbi}/share/opensbi/lp64/generic/firmware/fw_payload.bin
-          mkimage -f visionfive2-uboot-fit-image.its -A riscv -O u-boot -T firmware $out/visionfive2_fw_payload.img
+          cp ${uboot}/u-boot-spl.bin u-boot-spl.bin
+          spl_tool -c -f ./u-boot-spl.bin
+
+          install -Dm444 ./u-boot-spl.bin.normal.out $out/u-boot-spl.bin.normal.out
+          install -Dm444 ${uboot}/u-boot.itb $out/visionfive2_fw_payload.img
 
           runHook postInstall
         '';
       };
+
+      kernel = pkgs.callPackage ./pkgs/linux-vf2.nix { kernelPatches = [ ]; };
+
+      spl-tool = pkgs.callPackage
+        ({ lib, stdenv, fetchFromGitHub }: stdenv.mkDerivation rec {
+          pname = "spl-tool";
+          version = "1.0";
+
+          src = starfive-tools-src;
+
+          installPhase = ''
+            mkdir -p "$out/bin/"
+            cp spl_tool "$out/bin/"
+          '';
+
+          sourceRoot = "source/spl_tool";
+        })
+        {};
+
+      sd = nixosConfigurations.sd.config.system.build.sdImage;
     };
 
     devShells.x86_64-linux.default = let
@@ -175,7 +251,7 @@
    in pkgs.stdenv.mkDerivation {
       name = "VisionFive 2 Test";
 
-      nativeBuildInputs = [ pkgs.picocom ];
+      nativeBuildInputs = with pkgs; [ picocom zstd pv ];
     };
   };
 }
--- pkgs/linux-vf2.nix
+++ pkgs/linux-vf2.nix
@@ -1,27 +1,57 @@
 { lib , fetchFromGitHub , buildLinux, fetchpatch, ... } @ args:
-let
-  modDirVersion = "6.5.0-rc1";
-in
-buildLinux (args // {
-  inherit modDirVersion;
+buildLinux (args // rec {
+  modDirVersion = "6.6.0";
   version = "${modDirVersion}-vf2";
 
   src = fetchFromGitHub {
     owner = "starfive-tech";
     repo = "linux";
-    rev = "67e8df01b875afd312a7d2ab77f56a62f39dd6d9";
-    hash = "sha256-H+INnZTrfeCLmxPYbQEu3658/e5/Wz/Y5Ann2+lU6WQ=";
+    rev = "076ede06c00a4069cd9f90d609eaf35bf1bdc68a";
+    hash = "sha256-oI048iCkvghEIiuDHbxNHtJz/krPwXPB/HB85YUaxL8=";
   };
 
   structuredExtraConfig = with lib.kernel; {
     # According to starfive-tech/linux readme
-    ARCH_STARFIVE = yes;
-    SOC_STARFIVE = yes;
+    CPU_FREQ = yes;
+    CPUFREQ_DT = yes;
+    CPUFREQ_DT_PLATDEV = yes;
+    DMADEVICES = yes;
+    GPIO_SYSFS = yes;
+    HIBERNATION = yes;
+    NO_HZ_IDLE = yes;
+    POWER_RESET_GPIO_RESTART = yes;
+    PROC_KCORE = yes;
+    PWM = yes;
+    PWM_STARFIVE_PTC = yes;
+    RD_GZIP = yes;
+    SENSORS_SFCTEMP = yes;
     SERIAL_8250_DW = yes;
-    PINCTL_STARFIVE_JH7110_SYS = yes;
-    MMC_DW_STARFIVE = yes;
-    DWMAC_STARFIVE = yes;
+    SIFIVE_CCACHE = yes;
+    SIFIVE_PLIC = yes;
+
+    RTC_DRV_STARFIVE = yes;
+    SPI_PL022 = yes;
+    SPI_PL022_STARFIVE = yes;
+
+    I2C = yes;
+    MFD_AXP20X = yes;
+    MFD_AXP20X_I2C = yes;
+    REGULATOR_AXP20X = yes;
+
+    DRM = yes;
+    DRM_VERISILICON = yes;
+    STARFIVE_HDMI = yes;
+
+    PL330_DMA = no;
+
+    # Disable some drivers that we don't need to make the build leaner
+    NET_VENDOR_MELLANOX = no;
+    NET_VENDOR_MARVELL = no;
+    DRM_NOUVEAU = no;
+    DRM_INTEL = no;
   };
+
+  preferBuiltin = true;
 
   extraMeta = {
     branch = "JH7110_VisionFive2_upstream";
